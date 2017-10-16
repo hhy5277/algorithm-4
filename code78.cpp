9d37f71debdc33ef11e95783bf2e2d7937a4d938
hantaohuang <2812240764@qq.com>
2017-10-16 23:00:05 +0800

    统计二进制中1的个数--快速法

---
+++ code78.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    cout<<"input int a"<<endl;
+    cin>>n;
+
+    int count = 0;
+
+    while (n > 0) {
+        n = n & (n - 1);
+        count++;
+    }
+
+    cout<<count<<endl;
+
+    return 0;
+}
