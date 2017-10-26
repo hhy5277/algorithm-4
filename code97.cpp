2a5accd93426ef334a0969781ac23c3cb8213340
hantaohuang <2812240764@qq.com>
2017-10-26 17:28:20 +0800

    update

--- code97.cpp
+++ code97.cpp
@@ -39,13 +39,13 @@ public:
                                        *     
 class Solution {                       * 
 public:                                *
-    int mySqrt(int x) {                * 
-        long root = x;                 *
-        while (root*root > x) {        * 
-            root = (root + x/root) / 2;*
-        }                              *
-        return root;                   *
-    }                                  *
+	int mySqrt(int x) {                * 
+	    long root = x;                 *
+	    while (root*root > x) {        * 
+	        root = (root + x/root) / 2;*
+	    }                              *
+	    return root;                   *
+	}                                  *
 };                                     *
 ****************************************
 */ 
