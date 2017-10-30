90af5d8fba47e088f12667982b806a618e5c0284
hantaohuang <2812240764@qq.com>
2017-10-30 10:12:03 +0800

    剑指offer-面试题15：二进制中1的个数

---
+++ code112.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int NumberOf1(int n) {
+        int count = 0;
+
+        while (n) {
+        	n = n & (n - 1);
+        	++count;
+        }
+
+        return count;
+    }
+};
