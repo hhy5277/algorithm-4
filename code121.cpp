b2643e4e078a831385422422b84876cb1e9bf8df
hantaohuang <2812240764@qq.com>
2017-11-03 14:14:26 +0800

    剑指offer-面试题27：二叉树的镜像

---
+++ code121.cpp
@@ -0,0 +1,43 @@
+/*
+题目描述
+操作给定的二叉树，将其变换为源二叉树的镜像。
+输入描述:
+二叉树的镜像定义：源二叉树 
+    	    8
+    	   /  \
+    	  6   10
+    	 / \  / \
+    	5  7 9 11
+    	镜像二叉树
+    	    8
+    	   /  \
+    	  10   6
+    	 / \  / \
+    	11 9 7  5
+*/
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+    void Mirror(TreeNode *pRoot) {
+        if (NULL == pRoot) {
+        	return ;
+        }
+
+        swap(pRoot->left, pRoot->right);
+
+        if (pRoot->left != NULL) {
+        	Mirror(pRoot->left);
+        }
+        if (pRoot->right != NULL) {
+        	Mirror(pRoot->right);
+        }
+    }
+};
