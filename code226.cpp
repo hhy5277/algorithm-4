8325aa759e7b6e1ec12cba4e377100063482e845
huanghantao <2812240764@qq.com>
2018-02-08 16:12:14 +0800

    95. Unique Binary Search Trees II

---
+++ code226.cpp
@@ -0,0 +1,54 @@
+/*
+Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1...n.
+
+For example,
+Given n = 3, your program should return all 5 unique BST's shown below.
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+*/
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode *> generateTree(int from, int to)
+{
+    vector<TreeNode *> ret;
+    if(to - from < 0) ret.push_back(0);
+    if(to - from == 0) ret.push_back(new TreeNode(from));
+    if(to - from > 0)
+    {
+        for(int i=from; i<=to; i++)
+        {
+            vector<TreeNode *> l = generateTree(from, i-1);
+            vector<TreeNode *> r = generateTree(i+1, to);
+
+            for(int j=0; j<l.size(); j++)
+            {
+                for(int k=0; k<r.size(); k++)
+                {
+                    TreeNode * h = new TreeNode (i);
+                    h->left = l[j];
+                    h->right = r[k];
+                    ret.push_back(h);
+                }
+            }
+        }
+    }
+    return ret;
+}
+
+vector<TreeNode *> generateTrees(int n) {
+    return generateTree(1, n);
+}
+};
